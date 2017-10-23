9855ad6e741d6ac623287c3f35ec82e8dd78c57a
Ron Kurr <kurr@kurron.org>
2017-10-23 15:43:58 -0400

    Export the gateway ids so the bindings can use the values

--- debug/plan.tf
+++ debug/plan.tf
@@ -10,3 +10,11 @@ module "api_gateway" {
     api_name        = "Debug API"
     api_description = "A faux API just to test out the Terraform module"
 }
+
+output "api_gateway_id" {
+    value = "${module.api_gateway.api_gateway_id}"
+}
+
+output "api_gateway_root_resource_id" {
+    value = "${module.api_gateway.api_gateway_root_resource_id}"
+}
