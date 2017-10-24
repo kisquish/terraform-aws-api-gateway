f0ad62b3d9cdf7acca6dd3084cbacc11ddc2740f
Ron Kurr <kurr@kurron.org>
2017-10-24 15:26:33 -0400

    Map the path to the APIs distribution

--- debug/plan.tf
+++ debug/plan.tf
@@ -9,6 +9,9 @@ module "api_gateway" {
     region          = "us-west-2"
     api_name        = "Debug API"
     api_description = "A faux API just to test out the Terraform module"
+    domain_name     = "debug.transparent.engineering"
+    certificate_arn = "arn:aws:acm:us-east-1:037083514056:certificate/6e19dd75-e90d-4a83-a05e-2dc4bb171b4d"
+    hosted_zone_id  = "Z182ME1672NUSH"
 }
 
 output "api_gateway_id" {
--- main.tf
+++ main.tf
@@ -4,10 +4,31 @@ terraform {
 }
 
 provider "aws" {
-    region     = "${var.region}"
+    region = "${var.region}"
 }
 
 resource "aws_api_gateway_rest_api" "rest_api" {
     name        = "${var.api_name}"
     description = "${var.api_description}"
+}
+
+resource "aws_api_gateway_domain_name" "domain_name" {
+    domain_name     = "${var.domain_name}"
+    certificate_arn = "${var.certificate_arn}"
+}
+
+resource "aws_route53_record" "alias_record" {
+    zone_id = "${var.hosted_zone_id}"
+    name    = "${aws_api_gateway_domain_name.domain_name.domain_name}"
+    type = "A"
+    alias {
+        name                   = "${aws_api_gateway_domain_name.domain_name.cloudfront_domain_name}"
+        zone_id                = "${aws_api_gateway_domain_name.domain_name.cloudfront_zone_id}"
+        evaluate_target_health = true
+    }
+}
+
+resource "aws_api_gateway_base_path_mapping" "development" {
+    domain_name = "${aws_api_gateway_domain_name.domain_name.domain_name}"
+    api_id      = "${aws_api_gateway_rest_api.rest_api.id}"
 }
