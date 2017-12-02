135f759dd58475de060926d034cff312118eaaeb
Ron Kurr <kurr@kurron.org>
2017-12-02 12:01:16 -0500

    Do not create a base path mapping.  That needs to be done per resource now.

--- main.tf
+++ main.tf
@@ -27,8 +27,3 @@ resource "aws_route53_record" "alias_record" {
         evaluate_target_health = true
     }
 }
-
-resource "aws_api_gateway_base_path_mapping" "development" {
-    domain_name = "${aws_api_gateway_domain_name.domain_name.domain_name}"
-    api_id      = "${aws_api_gateway_rest_api.rest_api.id}"
-}
